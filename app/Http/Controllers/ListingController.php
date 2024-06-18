6c2c3577c634f537d34a7127b962a4685018756d
wahyudedik <wdedyk@gmail.com>
2024-06-18 08:42:51 +0700

    modified:   app/Http/Controllers/ListingController.php 	modified:   app/Models/Listing.php 	new file:   resources/views/listings/show.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,21 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\Listing;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ListingController extends Controller
 {
-    public function index(){
+    public function index(Request $request)
+    {
         $listings = Listing::where('is_active', true)
-        ->with('tags')
-        ->latest()
-        ->get();
+            ->with('tags')
+            ->latest()
+            ->get();
 
         $tags = Tag::orderBy('name')
-        ->get();
+            ->get();
+
+        if ($request->filled('s')) {
+            $query = strtolower($request->get('s'));
+            $listings = $listings->filter(function ($listing) use ($query) {
+                $searchableFields = [
+                    'title',
+                    'company',
+                    'location',
+                    'content',
+                ];
+
+                foreach ($searchableFields as $field) {
+                    if (Str::contains(strtolower($listing->$field), $query)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            });
+        }
+
+        if ($request->filled('tag')) {
+            $tag = $request->get('tag');
+            $listings = $listings->filter(function ($listing) use ($tag) {
+                return $listing->tags->contains('slug', $tag);
+            });
+        }
 
         return view('listings.index', compact('listings', 'tags'));
     }
+
+    public function show(Listing $listing, Request $request)
+    {
+        return view('listings.show', compact('listing'));
+    }
+
+    public function apply(Listing $listing, Request $request)
+    {
+        $listing->clicks()->create([
+            'user_agent' => $request->userAgent(),
+            'ip' => $request->ip(),
+        ]);
+
+        return redirect()->to($listing->apply_link);
+    }
+
+    public function create()
+    {
+        return view('listings.create');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ProfileController;
 
-route::get('/', [ListingController::class, 'index'])->name('listings.index');
+Route::get('/', [ListingController::class, 'index'])->name('listings.index');
+Route::get('/{listing}', [ListingController::class,'show'])->name('listings.show');
+Route::get('/{listing}/apply', [ListingController::class, 'apply'])->name('listings.apply');
+
+Route::get('/new', [ListingController::class, 'create'])->name('listings.create');
+Route::post('/new', [ListingController::class,'store'])->name('listings.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
