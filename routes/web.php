ca7f7770114a2383e8c50ad35961a3b82104218a
wahyudedik <wdedyk@gmail.com>
2024-06-22 13:48:32 +0700

    modified:   app/Http/Controllers/ListingController.php 	modified:   resources/views/components/header.blade.php 	new file:   resources/views/components/input.blade.php 	new file:   resources/views/components/label.blade.php 	modified:   resources/views/dashboard.blade.php 	new file:   resources/views/listings/create.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,27 @@
 
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
-Route::get('/{listing}', [ListingController::class,'show'])->name('listings.show');
-Route::get('/{listing}/apply', [ListingController::class, 'apply'])->name('listings.apply');
-
 Route::get('/new', [ListingController::class, 'create'])->name('listings.create');
-Route::post('/new', [ListingController::class,'store'])->name('listings.store');
+Route::post('/new', [ListingController::class, 'store'])->name('listings.store');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard', function (Request $request) {
+    return view('dashboard', [
+        'listings' => $request->user()->listings
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/{listing}', [ListingController::class,'show'])->name('listings.show');
+Route::get('/{listing}/apply', [ListingController::class, 'apply'])->name('listings.apply');
 
-require __DIR__.'/auth.php';
